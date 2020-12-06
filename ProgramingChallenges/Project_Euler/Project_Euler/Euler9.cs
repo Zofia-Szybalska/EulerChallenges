c6bbc76de0ec16fd0a1c46a97e1a9dc93602917f
Zofia Szybalska <zochula111@gmail.com>
2020-12-06 22:44:51 +0100

    Edit

---
+++ ProgramingChallenges/Project_Euler/Project_Euler/Euler9.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_Euler
+{
+    class Euler9
+    {
+        public static void Calculate()
+        {
+
+        }
+    }
+}
--- ProgramingChallenges/Project_Euler/Project_Euler/Program.cs
+++ ProgramingChallenges/Project_Euler/Project_Euler/Program.cs
@@ -8,13 +8,52 @@ namespace Project_Euler
 
         static void Main(string[] args)
         {
-            Euler1.Calculate();
-            Euler2.Calculate();
-            Euler3.Calculate();
-            Euler4.Calculate();
-            Euler5.Calculate();
-            Euler6.Calculate();
-            Euler7.Calculate();
+            Console.WriteLine("With Euler do you want to run? \nEnter number from 1 to 8.");
+            int euler; 
+            etykieta:
+            string temp = Console.ReadLine();
+            bool check = Int32.TryParse(temp, out euler);
+            
+                switch (euler)
+                {
+                    case 1:
+                        Euler1.Calculate();
+                        break;
+                    case 2:
+                        Euler2.Calculate();
+                        break;
+                    case 3:
+                        Euler3.Calculate();
+                        break;
+                    case 4:
+                        Euler4.Calculate();
+                        break;
+                    case 5:
+                        Euler5.Calculate();
+                        break;
+                    case 6:
+                        Euler6.Calculate();
+                        break;
+                    case 7:
+                        Euler7.Calculate();
+                        break;
+                    case 8:
+                        Euler8.Calculate();
+                        break;
+                    case 9:
+                        Euler9.Calculate();
+                        break;
+
+                    default:
+                        Console.WriteLine("Enter number from 1 to 8, please.");
+                        goto etykieta;
+
+                }
+
+            
+           
+
+            
 
         }
     }
